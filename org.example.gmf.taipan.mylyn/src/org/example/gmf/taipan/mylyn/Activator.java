33d2ded4d2db19d3c6fe7e38aa4ae1656d07b8d1
Manuel Bork <bork@yatta.de>
2012-06-18 17:18:31 +0200

    connect Mylyn's selection monitor to the new bridges

--- org.example.gmf.taipan.mylyn/src/org/example/gmf/taipan/mylyn/Activator.java
+++ org.example.gmf.taipan.mylyn/src/org/example/gmf/taipan/mylyn/Activator.java
@@ -1,5 +1,10 @@
 package org.example.gmf.taipan.mylyn;
 
+import org.eclipse.mylyn.context.core.AbstractContextStructureBridge;
+import org.eclipse.mylyn.context.core.ContextCore;
+import org.eclipse.mylyn.context.ui.IContextUiStartup;
+import org.eclipse.mylyn.mft.emf.ui.DiagramUiEditingMonitor;
+import org.eclipse.mylyn.monitor.ui.MonitorUi;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -11,9 +16,17 @@ public class Activator extends AbstractUIPlugin {
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.example.gmf.taipan.mylyn"; //$NON-NLS-1$
 
+	public static class EcoreDiagramBridgeStartup implements IContextUiStartup {
+		public void lazyStartup() {
+			Activator.getDefault().lazyStart();
+		}
+	}
+
 	// The shared instance
 	private static Activator plugin;
-	
+
+	private DiagramUiEditingMonitor diagramMonitor;
+
 	/**
 	 * The constructor
 	 */
@@ -34,6 +47,7 @@ public class Activator extends AbstractUIPlugin {
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
+		lazyStop();
 		plugin = null;
 		super.stop(context);
 	}
@@ -47,4 +61,19 @@ public class Activator extends AbstractUIPlugin {
 		return plugin;
 	}
 
+	private void lazyStart() {
+		AbstractContextStructureBridge bridge = ContextCore
+				.getStructureBridge(PLUGIN_ID);
+		if (bridge instanceof TaipanContextStructureBridge) {
+			diagramMonitor = new DiagramUiEditingMonitor(bridge,
+					TaipanContextUiBridge.getInstance());
+			MonitorUi.getSelectionMonitors().add(diagramMonitor);
+		}
+	}
+
+	private void lazyStop() {
+		if (diagramMonitor != null) {
+			MonitorUi.getSelectionMonitors().remove(diagramMonitor);
+		}
+	}
 }
