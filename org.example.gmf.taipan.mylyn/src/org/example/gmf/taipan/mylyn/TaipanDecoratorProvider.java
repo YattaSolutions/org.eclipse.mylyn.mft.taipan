66baf4c3aa18da88efabf617e4bc1db34f943e65
Manuel Bork <bork@yatta.de>
2012-06-18 17:08:09 +0200

    created and registered decorator provider

---
+++ org.example.gmf.taipan.mylyn/src/org/example/gmf/taipan/mylyn/TaipanDecoratorProvider.java
@@ -0,0 +1,18 @@
+package org.example.gmf.taipan.mylyn;
+
+import org.eclipse.mylyn.mft.emf.ui.DiagramUiBridge;
+import org.eclipse.mylyn.mft.gmf.ui.ContextDecoratorProvider;
+
+/**
+ * The decorator provider connects to the UI bridge.
+ * 
+ * @author <a href="mailto:bork@yatta.de">Manuel Bork</a>
+ */
+public class TaipanDecoratorProvider extends ContextDecoratorProvider {
+
+	@Override
+	public DiagramUiBridge getDomainUIBridge() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
