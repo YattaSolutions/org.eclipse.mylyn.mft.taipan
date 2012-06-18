82c25d9907a6da1a17c5b84ebf7a83f4b97f908f
Manuel Bork <bork@yatta.de>
2012-06-18 17:00:51 +0200

    created context ui bridge

---
+++ org.example.gmf.taipan.mylyn/src/org/example/gmf/taipan/mylyn/TaipanContextUiBridge.java
@@ -0,0 +1,35 @@
+package org.example.gmf.taipan.mylyn;
+
+import org.eclipse.gmf.examples.taipan.gmf.editor.part.TaiPanDiagramEditor;
+import org.eclipse.mylyn.mft.emf.ui.DiagramUiBridge;
+import org.eclipse.ui.IWorkbenchPart;
+
+/**
+ * The purpose of the UI bridge is to define which UI views and editors should
+ * be supported by MFT.<br>
+ * The implementation will be responsible for the Taipan content type and should
+ * handle all Taipan implementations of IWorkbenchPart, which is just the
+ * TaiPanDiagramEditor. <br>
+ * More fine-grained control is available to decide which combination of model
+ * and view instances are related to each other, which is simple in the Taipan
+ * example, since each model object has exactly one view representation.
+ * 
+ * @author <a href="mailto:bork@yatta.de">Manuel Bork</a>
+ */
+public class TaipanContextUiBridge extends DiagramUiBridge {
+	@Override
+	public boolean acceptsPart(IWorkbenchPart part) {
+		// is this WorkbenchPart supported?
+		return part instanceof TaiPanDiagramEditor;
+	}
+
+	@Override
+	public boolean acceptsViewObject(Object modelObject, Object viewObject) {
+		return true;
+	}
+
+	@Override
+	public String getContentType() {
+		return Activator.PLUGIN_ID;
+	}
+}
